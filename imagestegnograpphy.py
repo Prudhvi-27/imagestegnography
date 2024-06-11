d8bc05b9c94e8d52f0cbd998c8998ceb54d3a70d
PrudhviRaj <120821179+Prudhvi-27@users.noreply.github.com>
2024-06-11 23:04:21 +0530

    Create imagestegnograpphy.py

---
+++ imagestegnograpphy.py
@@ -0,0 +1,21 @@
+Steganography Image Encryption and Decryption
+Description
+This Python project utilizes the Tkinter library for GUI, allowing users to encode and decode hidden messages within images using steganography techniques.
+
+Features
+Encode Functionality: Allows users to embed text data into an image.
+Decode Functionality: Extracts hidden messages from encoded images.
+Installation
+Requirements: Ensure you have Python installed.
+Libraries: Install necessary libraries using: pip istall pillow
+Usage
+Run the main.py file.
+Choose between encoding or decoding options in the GUI.
+Encoding:
+Enter the path of the image to encode.
+Enter the text to be encoded.
+Specify the desired name for the output image.
+Click on "Encode the Image" to generate the encoded image.
+Decoding:
+Enter the path of the encoded image.
+Click on "Decode the Image" to retrieve the hidden message. https://github.com/METTUYAMINI/imagesteganography/assets/143019561/2bfdc611-cb1b-4714-93e2-1887f64dd117
